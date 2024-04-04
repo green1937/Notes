2175751cac793af2ecff6cf3a23011843ce928eb
Sofia <sofiy.gavrilenko@gmail.com>
2024-04-04 18:05:31 +0700

    Реализована сортировка заметок

--- app/src/main/java/com/example/dma/MainActivity.java
+++ app/src/main/java/com/example/dma/MainActivity.java
@@ -7,6 +7,9 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 import com.google.android.material.button.MaterialButton;
 
@@ -15,9 +18,12 @@ import io.realm.RealmChangeListener;
 import io.realm.RealmResults;
 
 public class MainActivity extends AppCompatActivity {
-
+    RealmResults<Note> notesList; // глобальная переменная
+    RadioButton sortByTitle, sortByTime;
+    Button submit;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
@@ -32,21 +38,86 @@ public class MainActivity extends AppCompatActivity {
         });
 
         Realm.init(getApplicationContext());
-        Realm realm = Realm.getDefaultInstance();
 
-        RealmResults<Note> notesList = realm.where(Note.class).findAll();
+
+        sortByTitle = (RadioButton) findViewById(R.id.titleSort);
+        sortByTime = (RadioButton) findViewById(R.id.timeSort);
+        submit = (Button) findViewById(R.id.sortBtn);
 
         RecyclerView recyclerView = findViewById(R.id.recyclerview);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        Realm realm = Realm.getDefaultInstance();
+        notesList = realm.where(Note.class).findAll();
         MyAdapter myAdapter = new MyAdapter(getApplicationContext(), notesList);
         recyclerView.setAdapter(myAdapter);
-
         notesList.addChangeListener(new RealmChangeListener<RealmResults<Note>>() {
             @Override
             public void onChange(RealmResults<Note> notes) {
+
                 myAdapter.notifyDataSetChanged();
             }
         });
 
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (sortByTitle.isChecked()) {
+                    Realm realm = Realm.getDefaultInstance();
+                    notesList = realm.where(Note.class).findAll().sort("title");
+                    MyAdapter myAdapter = new MyAdapter(getApplicationContext(), notesList);
+                    recyclerView.setAdapter(myAdapter);
+                    notesList.addChangeListener(new RealmChangeListener<RealmResults<Note>>() {
+                        @Override
+                        public void onChange(RealmResults<Note> notes) {
+
+                            myAdapter.notifyDataSetChanged();
+                        }
+                    });
+                } else if (sortByTime.isChecked()) {
+                    Realm realm = Realm.getDefaultInstance();
+                    notesList = realm.where(Note.class).findAll();
+                    MyAdapter myAdapter = new MyAdapter(getApplicationContext(), notesList);
+                    recyclerView.setAdapter(myAdapter);
+                    notesList.addChangeListener(new RealmChangeListener<RealmResults<Note>>() {
+                        @Override
+                        public void onChange(RealmResults<Note> notes) {
+
+                            myAdapter.notifyDataSetChanged();
+                        }
+                    });
+                }
+            }
+        });
+
+
+       // notesList = realm.where(Note.class).findAll();
+
+
+
+
+
+
+
+
+
+    }
+    private RealmResults<Note> sortAndReturnNotes() {
+        Realm realm = Realm.getDefaultInstance();
+        realm.beginTransaction();
+
+        RadioGroup sortGr = findViewById(R.id.rgSort);
+        sortGr.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                if(checkedId == R.id.titleSort) {
+                    notesList = realm.where(Note.class).findAll().sort("title");
+
+                }
+                else {
+                    notesList = realm.where(Note.class).findAll();
+                }
+            }
+        });
+        return notesList;
     }
 }
